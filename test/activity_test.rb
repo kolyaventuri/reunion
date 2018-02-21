49ace21e251c884a8c37a01bfad10f6d78e27303
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 09:14:29 -0700

    Initial Activity tests

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -3,4 +3,13 @@ require './test/test_helper'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_can_create_activity
+    activity = Activity.new 'Foo'
+    assert_instance_of Activity, activity
+  end
+
+  def test_activity_can_have_name
+    activity = Activity.new 'Hiking'
+    assert_equal 'Hiking', activity.name
+  end
 end
