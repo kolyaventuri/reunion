bd985d719885a444c86b52fdac939f1f79ad25b0
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 10:35:38 -0700

    Added reunion generation and expected HTML output

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -12,4 +12,18 @@ class RendererTest < Minitest::Test
   def test_does_create_renderer
     assert_instance_of Renderer, @renderer
   end
+
+  def setup_reunion
+    activity = Activity.new 'Frisbee', 5.0
+    activity2 = Activity.new 'Singalong', 2.0
+
+    activity.add_participant(name: 'Sue', amt_paid: 5.0)
+    activity.add_participant(name: 'Bob', amt_paid: 3.0)
+
+    activity2.add_participant(name: 'Sue', amt_paid: 3.0)
+    activity2.add_participant(name: 'Bob', amt_paid: 2.0)
+
+    @reunion.add_activity activity2
+    @reunion.add_activity activity
+  end
 end
