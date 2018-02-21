0fcada6aaf3c1e910cfa57bbeeaf23811b36a07d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 10:41:18 -0700

    Renderer#render method

--- lib/Renderer.rb
+++ lib/Renderer.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
+require 'erb'
 
 # Takes ERB templates and renders HTML
 class Renderer
   attr_reader :reunion
+  TEMPLATE = File.read('./data/template.erb')
 
   def initialize(reunion)
     @reunion = reunion
+  end
+
+  def render
+    renderer = ERB.new TEMPLATE
+    renderer.result
   end
 end
