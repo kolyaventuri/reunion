c1aed5eb85132adec87e72ad6cfe310a9ac9919f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 09:52:13 -0700

    Test for reunion split cost method

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -33,12 +33,23 @@ class ReunionTest < Minitest::Test
     assert_equal 14.0, @reunion.total_cost
   end
 
+  def test_can_calculate_split_per_person
+    setup_reunion
+
+    expected = [
+      { name: 'Sue', amt_owed: -1.0 },
+      { name: 'Bob', amt_owed: 2.0 }
+    ]
+
+    assert_equal expected, @reunion.split_cost
+  end
+
   def setup_reunion
     activity = Activity.new 'Frisbee', 5.0
     activity2 = Activity.new 'Singalong', 2.0
 
     activity.add_participant(name: 'Sue', amt_paid: 5.0)
-    activity.add_participant(name: 'Bob', amt_paid: 4.0)
+    activity.add_participant(name: 'Bob', amt_paid: 3.0)
 
     activity2.add_participant(name: 'Sue', amt_paid: 3.0)
     activity2.add_participant(name: 'Bob', amt_paid: 2.0)
