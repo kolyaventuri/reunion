aed4f7ed000940d0316ad7eaa0b03ed7cbef3e0f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 09:17:54 -0700

    Add participants test

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -3,13 +3,29 @@ require './test/test_helper'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
-  def test_can_create_activity
-    activity = Activity.new 'Foo'
-    assert_instance_of Activity, activity
+  def setup
+    @activity = Activity.new 'Hiking'
   end
 
-  def test_activity_can_have_name
-    activity = Activity.new 'Hiking'
-    assert_equal 'Hiking', activity.name
+  def test_can_create_activity
+    assert_instance_of Activity, @activity
+  end
+
+  def test_can_have_name
+    assert_equal 'Hiking', @activity.name
+  end
+
+  def test_can_add_participants
+    assert_equal [], @activity.participants
+
+    participant = { name: 'Sue', amt_paid: 10.0 }
+    assert_equal participant, @activity.add_participant(participant)
+
+    assert_equal [participant], @activity.participants
+
+    participant2 = { name: 'Bob', amt_paid: 12.0 }
+    assert_equal participant2, @activity.add_participant(participant2)
+
+    assert_equal [participant, participant2], @activity.participants
   end
 end
