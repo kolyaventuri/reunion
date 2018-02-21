5c1bb5e443efaf0a44fe01b261c2657c2b1e73af
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 09:19:19 -0700

    Activity#add_participant implemented

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,8 +1,15 @@
 # Defines an activity for the reunion
 class Activity
-  attr_reader :name
+  attr_reader :name,
+              :participants
 
   def initialize(name)
     @name = name
+    @participants = []
+  end
+
+  def add_participant(participant)
+    @participants.push participant
+    participant
   end
 end
