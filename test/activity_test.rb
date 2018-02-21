3d0179539a3e24beeff1a15c6b8d283896f8ffc9
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 09:20:39 -0700

    Total cost tests

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -28,4 +28,16 @@ class ActivityTest < Minitest::Test
 
     assert_equal [participant, participant2], @activity.participants
   end
+
+  def test_can_evaluate_total_cost
+    participant = { name: 'Sue', amt_paid: 10.0 }
+    @activity.add_participant(participant)
+
+    assert_equal 10.0, @activity.total_cost
+
+    participant2 = { name: 'Bob', amt_paid: 12.5 }
+    @activity.add_participant(participant2)
+
+    assert_equal 22.5, @activity.total_cost
+  end
 end
