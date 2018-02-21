6ebb9419c61506e44558066ece36cfdfdbdbcde7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 09:38:15 -0700

    Base reunion tests

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -5,4 +5,11 @@ require './test/test_helper'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def setup
+    @reunion = Reunion.new 'Beach'
+  end
+
+  def test_can_create_runion
+    assert_instance_of Runion, reunion
+  end
 end
